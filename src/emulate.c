f02f179fdcc7a940b87a8d386617c14a3e02b761
Alex R <arr19@ic.ac.uk>
2020-05-25 14:30:11 +0100

    Initial commit

--- src/emulate.c
+++ src/emulate.c
@@ -1,31 +1,67 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <math.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-#define MAX_MEMORY = 65536;
-#define REG_NUMBER = 17;
+#define MEMORY_CAPACITY 65536
+#define NO_REGISTERS 17
+#define ADDRESS_SIZE 4
+#define BITS_SET(value, mask, bits)((value & mask) == bits)
 
+typedef uint32_t word;
 
-//binary files are non-text files
-//which have permanent storage capabilites
+/*registers 0-12 will be used by their value so for reg0 we can just use 0
+but these will make it easier to address in memory*/
+enum Register {PC = 15, CPSR = 16};
 
-//structure of binary file
-struct record {
-    int a,b,c;
-};
+typedef struct {
+  // ARM machine memory
+  uint8_t * memory;
+  // 0-12 general purpose, 13 SP, 14 LR, 15 PC, 16 CPSR
+  word * registers;
+} arm;
 
-//reads binary file and returns a char array with its contents
-char* binaryFileReader(char* fname){
-    struct record r haa;
-    File *fptr;
-    fptr = fopen(fname, "rb");
-    fread(buffer,sizeof(buffer), 1 ,fptr);
-
+void ptrValidate(const void * pointer, char* error) {
+  if (pointer == NULL) {
+    printf("Error: %s\n", error);
+    exit(EXIT_FAILURE);
+  }
 }
 
+void instructionSelect(word instruction) {
+  const word dpMask = 0x0C000000;
+  const word dp = 0x00000000;
+  const word multMask = 0x0FC000F0;
+  const word mult = 0x0000090;
+  const word sdtMask = 0x0C600000;
+  const word sdt = 0x04000000;
+  const word branchMask = 0x0F000000;
+  const word branch = 0x0A000000;
 
-int main(int argc, char **argv) {
+  // TODO: determine how to differentiate ...
+  // ... `data processing` from `multiply`
+
+  if (BITS_SET(instruction, branchMask, branch)) {
+    // function for branch instructions
+  } else if (BITS_SET(instruction, sdtMask, sdt)) {
+    // function for single data tranfser instructions
+  } else if (BITS_SET(instruction, multMask, mult)) {
+    // function for multiply instructions
+  } else if (BITS_SET(instruction, dpMask, dp)) {
+    // function for data processing instructions
+  }
+}
+
+int main(int argc, char ** argv) {
+  if (argc == 1) {
+		printf("Please specify an ARM binary object code file.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	arm * state;
+
+  // free memory before code termination
+	free(state -> memory);
+	free(state -> registers);
+	free(state);
   return EXIT_SUCCESS;
 }
