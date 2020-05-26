cb79006a5db2c13ed7d06ed58ca79c534fb393e6
Luqman <lml19@imperial.ac.uk>
2020-05-26 17:43:20 +0100

    Replaced magic numbers with constants

--- src/emulate.c
+++ src/emulate.c
@@ -1,17 +1,39 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+// Constants will be moved into a seperate header file
+// general constants
 #define MEMORY_CAPACITY 65536
 #define NO_REGISTERS 17
 #define ADDRESS_SIZE 4
 #define BITS_SET(value, mask, bits) ((value & mask) == bits)
+
+// Constants for Multiply
+#define MULT_RDEST_MASK 0x000F0000
+#define MULT_REG_S_MASK 0x00000F00
+#define MULT_REG_M_MASK 0x0000000F
+#define MULT_REG_N_MASK 0x0000F000
+#define MULT_REG_S_SHIFT 8
+#define MULT_REG_N_SHIFT 12
+#define MULT_RDEST_SHIFT 16
+#define ACCUMULATE_FLAG (1 << 21)
+#define UPDATE_CPSR (1 << 20)
+#define CPSR_N (1 << 31)
+#define CPSR_Z (1 << 30)
 
 typedef uint32_t word;
 
 /*registers 0-12 will be used by their value so for reg0 we can just use 0
 but these will make it easier to address in memory*/
 enum Register { PC = 15, CPSR = 16 };
+// opcode mnemonics
+enum Opcode { AND, EOR, SUB, RSB, ADD, TST = 8, TEQ, CMP, ORR = 12, MOV };
+// condition suffixes
+enum Cond { EQ, NE, GE = 10, LT, GT, LE, AL };
+// shift types
+enum Shift { LSL, LSR, ASR, ROR };
 
 typedef struct {
   // ARM machine memory
@@ -27,13 +49,13 @@ void ptrValidate(const void *pointer, char *error) {
   }
 }
 
-// From Alex
+// From Alex with shifts included
 bool checkCond(arm state, word instruction) {
   // CPSR flag bits
-  unsigned int n = state.registers[CPSR] & 0x80000000;
-  unsigned int z = state.registers[CPSR] & 0x40000000;
-  unsigned int c = state.registers[CPSR] & 0x20000000;
-  unsigned int v = state.registers[CPSR] & 0x10000000;
+  unsigned int n = (state.registers[CPSR] & 0x80000000) >> 31;
+  unsigned int z = (state.registers[CPSR] & 0x40000000) >> 30;
+  unsigned int c = (state.registers[CPSR] & 0x20000000) >> 29;
+  unsigned int v = (state.registers[CPSR] & 0x10000000) >> 28;
   unsigned int cond = instruction >> 28;
   // conditions for instruction
   switch (cond) {
@@ -57,35 +79,32 @@ bool checkCond(arm state, word instruction) {
 }
 
 void multiply(arm state, word instruction) {
-  if (!checkCond(state, instruction))
+  // no execution if condition code does not match
+  if (!checkCond(state, instruction)) {
     return;
-  int result;
-
+  }
   // Extraction of information from the instruction;
-  int destination = (instruction & 0x000F0000) >> 16;
-  int regS = (instruction & 0x00000F00) >> 8;
-  int regM = instruction & 0x0000000F;
-  result = state.registers[regM] * state.registers[regS];
+  int destination = (instruction & MULT_RDEST_MASK) >> MULT_RDEST_SHIFT;
+  int regS = (instruction & MULT_REG_S_MASK) >> MULT_REG_S_SHIFT;
+  int regM = instruction & MULT_REG_M_MASK;
+
+  // Initial execution of instruction
+  int result = state.registers[regM] * state.registers[regS];
 
   // Obtain value from Rn and add to result if Accumulate is set
-  // need to confirm arguments of BITS_SET with Alex
-  if (BITS_SET(instruction, 1 << 21, 1 << 21)) {
-    int regN = (instruction & 0x0000F000) >> 12;
+  if (BITS_SET(instruction, ACCUMULATE_FLAG, ACCUMULATE_FLAG)) {
+    int regN = (instruction & MULT_REG_N_MASK) >> MULT_REG_N_SHIFT;
     result += state.registers[regN];
   }
   // Update CPSR flags if S (bit 20 in instruction) is set
-  if (BITS_SET(instruction, 1 << 20, 1 << 20)) {
-    state.registers[CPSR] |= (result & (1 << 31));
+  if (BITS_SET(instruction, UPDATE_CPSR, UPDATE_CPSR)) {
+    state.registers[CPSR] |= (result & CPSR_N);
     if (!result)
-      state.registers[CPSR] |= (1 << 30);
+      state.registers[CPSR] |= CPSR_Z;
   }
 
   // write result to destination register
   state.registers[destination] = result;
-
-  /*TO DO:
-    - Possibly figure out way of reducing magic number masks
-  */
 }
 
 void decode(arm state, word instruction) {
