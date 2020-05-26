900955ab2c8de6654901f27b7b55cabc2e8780ef
Alex R <arr19@ic.ac.uk>
2020-05-26 15:34:09 +0100

    Register Operand2 functions added

--- src/emulate.c
+++ src/emulate.c
@@ -1,7 +1,7 @@
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
 
 #define MEMORY_CAPACITY 65536
 #define NO_REGISTERS 17
@@ -9,6 +9,7 @@
 #define BITS_SET(value, mask, bits) ((value & mask) == bits)
 
 typedef uint32_t word;
+typedef unsigned int uint;
 
 /*registers 0-12 will be used by their value so for reg0 we can just use 0
 but these will make it easier to address in memory*/
@@ -17,12 +18,14 @@ enum Register { PC = 15, CPSR = 16 };
 enum Opcode { AND, EOR, SUB, RSB, ADD, TST = 8, TEQ, CMP, ORR = 12, MOV };
 // condition suffixes
 enum Cond { EQ, NE, GE = 10, LT, GT, LE, AL };
+// shift types
+enum Shift { LSL, LSR, ASR, ROR };
 
 typedef struct {
   // ARM machine memory
-  uint8_t * memory;
+  uint8_t *memory;
   // 0-12 general purpose, 13 SP, 14 LR, 15 PC, 16 CPSR
-  word * registers;
+  word *registers;
 } arm;
 
 void ptrValidate(const void *pointer, char *error) {
@@ -34,30 +37,76 @@ void ptrValidate(const void *pointer, char *error) {
 
 bool checkCond(arm state, word instruction) {
   // CPSR flag bits
-  unsigned int n = state.registers[CPSR] & 0x80000000;
-  unsigned int z = state.registers[CPSR] & 0x40000000;
-  unsigned int c = state.registers[CPSR] & 0x20000000;
-  unsigned int v = state.registers[CPSR] & 0x10000000;
-  unsigned int cond = instruction >> 28;
+  uint n = state.registers[CPSR] & 0x80000000;
+  uint z = state.registers[CPSR] & 0x40000000;
+  uint c = state.registers[CPSR] & 0x20000000;
+  uint v = state.registers[CPSR] & 0x10000000;
+  uint cond = instruction >> 28;
   // conditions for instruction
   switch (cond) {
-    case EQ:
-      return z;
-    case NE:
-      return !z;
-    case GE:
-      return n == v;
-    case LT:
-      return n != v;
-    case GT:
-      return !z && (n == v);
-    case LE:
-      return z || (n != v);
-    case AL:
-      return true;
-    default:
-      return false;
-    }
+  case EQ:
+    return z;
+  case NE:
+    return !z;
+  case GE:
+    return n == v;
+  case LT:
+    return n != v;
+  case GT:
+    return !z && (n == v);
+  case LE:
+    return z || (n != v);
+  case AL:
+    return true;
+  default:
+    return false;
+  }
+}
+
+uint shiftConstant(uint shiftPart) {
+  // integer specified by bits 7-4
+  return shiftPart >> 4;
+}
+
+uint shiftRegister(arm state, uint rs) {
+  // pre: Rs (register) can be any general purpose register except the PC
+  // bottom byte of Rs specifies the amount to be shifted
+  return state.registers[rs] & 0x0000000F;
+}
+
+uint shiftI(word value, uint shiftBy, uint shiftTypeBits) {
+  // TODO: set the CPSR flags
+  switch (shiftTypeBits) {
+  case LSL:
+    return value << shiftBy;
+  case LSR:
+    return value >> shiftBy;
+  case ASR:
+    // TODO: check if this is an arithmetic shift
+    return (signed int)value > shiftBy;
+  case ROR:
+    break;
+  }
+}
+
+int opRegister(arm state, uint op2) {
+  // bits indicating the shift instruction
+  uint shiftPart = op2 >> 4;
+  // number to shift by
+  uint shiftBy;
+  if (shiftPart & 0x01) {
+    // shift by a constant
+    shiftBy = shiftConstant(shiftPart);
+  } else {
+    // shift specified by a register (optional)
+    shiftBy = shiftRegister(state, shiftPart >> 4);
+  }
+  return shiftI(state.registers[op2 & 0x00F], shiftBy, (shiftPart & 0x06) >> 1);
+}
+
+int opImmediate(arm state, uint op2) {
+  // TODO: code me
+  return 0;
 }
 
 void dpi(arm state, word instruction) {
@@ -65,11 +114,11 @@ void dpi(arm state, word instruction) {
     return;
   }
   // parts of the instruction
-  unsigned int i = (instruction & 0x02000000) >> 25;
-  unsigned int s = (instruction & 0x00100000) >> 20;
-  unsigned int rn = (instruction & 0x000F0000) >> 16;
-  unsigned int rd = (instruction & 0x0000F000) >> 12;
-  unsigned int op2 = instruction & 0x00000FFF;
+  uint i = (instruction & 0x02000000) >> 25;
+  uint s = (instruction & 0x00100000) >> 20;
+  uint rn = (instruction & 0x000F0000) >> 16;
+  uint rd = (instruction & 0x0000F000) >> 12;
+  uint op2 = instruction & 0x00000FFF;
 
   // TODO:
   // If the S bit is 0, the CPSR register is unaffected
@@ -84,52 +133,60 @@ void dpi(arm state, word instruction) {
   // The N bit will be set to the logical value of bit 31 of the result.
 
   // TODO: operand2 is an immediate value
-  // TODO: operand2 is a register
 
+  // op2
+  uint value;
+  if (i) {
+    // op2 is an immediate value
+    value = opImmediate(state, op2);
+  } else {
+    // op2 is a register value
+    value = opRegister(state, op2);
+  }
   // execution of instruction
   enum Opcode opcode = instruction & 0x01E00000;
   switch (opcode) {
-    case AND:
-      // Rn AND operand2
-      state.registers[rd] = state.registers[rn] & op2;
-      break;
-    case EOR:
-      // Rn EOR operand2
-      state.registers[rd] = state.registers[rn] ^ op2;
-      break;
-    case SUB:
-      // Rn - operand2
-      state.registers[rd] = state.registers[rn] - op2;
-      break;
-    case RSB:
-      // operand2 - Rn
-      state.registers[rd] = op2 - state.registers[rn];
-      break;
-    case ADD:
-      // Rn + operand2
-      state.registers[rd] = state.registers[rn] + op2;
-      break;
-    case TST:
-      // as and, but result not written
-      state.registers[rn] & op2;
-      break;
-    case TEQ:
-      // as eor, but result is not written
-      state.registers[rd] = state.registers[rn] ^ op2;
-      break;
-    case CMP:
-      // as sub, but result is not written
-      state.registers[rd] = state.registers[rn] - op2;
-      break;
-    case ORR:
-      // Rn OR operand2
-      state.registers[rd] = state.registers[rn] | op2;
-      break;
-    case MOV:
-      // operand2 (Rn is ignored)
-      state.registers[rd] = op2;
-      break;
-    }
+  case AND:
+    // Rn AND operand2
+    state.registers[rd] = state.registers[rn] & op2;
+    break;
+  case EOR:
+    // Rn EOR operand2
+    state.registers[rd] = state.registers[rn] ^ op2;
+    break;
+  case SUB:
+    // Rn - operand2
+    state.registers[rd] = state.registers[rn] - op2;
+    break;
+  case RSB:
+    // operand2 - Rn
+    state.registers[rd] = op2 - state.registers[rn];
+    break;
+  case ADD:
+    // Rn + operand2
+    state.registers[rd] = state.registers[rn] + op2;
+    break;
+  case TST:
+    // as and, but result not written
+    state.registers[rn] & op2;
+    break;
+  case TEQ:
+    // as eor, but result is not written
+    state.registers[rd] = state.registers[rn] ^ op2;
+    break;
+  case CMP:
+    // as sub, but result is not written
+    state.registers[rd] = state.registers[rn] - op2;
+    break;
+  case ORR:
+    // Rn OR operand2
+    state.registers[rd] = state.registers[rn] | op2;
+    break;
+  case MOV:
+    // operand2 (Rn is ignored)
+    state.registers[rd] = op2;
+    break;
+  }
 }
 
 void decode(arm state, word instruction) {
@@ -156,12 +213,12 @@ void decode(arm state, word instruction) {
   }
 }
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
   if (argc == 1) {
     printf("Please specify an ARM binary object code file.\n");
     exit(EXIT_FAILURE);
   }
-  arm * state;
+  arm *state;
 
   // free memory before code termination
   free(state->memory);
