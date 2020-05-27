3d29e1023426077471878456f9ce03c738965646
Luqman <lml19@imperial.ac.uk>
2020-05-27 13:17:40 +0100

    Changed state parameter to be a pointer

--- src/emulate.c
+++ src/emulate.c
@@ -13,7 +13,7 @@
 // Constants for Branch
 #define BRANCH_OFFSET_MASK 0x00FFFFFF
 #define BRANCH_SIGN_BIT (1 << 23)
-#define PIPELINE_SHIFT 2
+#define CURRENT_INSTRUCTION_SHIFT 2
 #define NEGATIVE_SIGN_EXTEND 0xFC000000
 #define POSITIVE_SIGN_EXTEND 0
 
@@ -44,12 +44,12 @@ void ptrValidate(const void *pointer, char *error) {
 }
 
 // From Alex with shifts included
-bool checkCond(arm state, word instruction) {
+bool checkCond(arm *state, word instruction) {
   // CPSR flag bits
-  unsigned int n = (state.registers[CPSR] & 0x80000000) >> 31;
-  unsigned int z = (state.registers[CPSR] & 0x40000000) >> 30;
-  unsigned int c = (state.registers[CPSR] & 0x20000000) >> 29;
-  unsigned int v = (state.registers[CPSR] & 0x10000000) >> 28;
+  unsigned int n = (state->registers[CPSR] & 0x80000000) >> 31;
+  unsigned int z = (state->registers[CPSR] & 0x40000000) >> 30;
+  unsigned int c = (state->registers[CPSR] & 0x20000000) >> 29;
+  unsigned int v = (state->registers[CPSR] & 0x10000000) >> 28;
   unsigned int cond = instruction >> 28;
   // conditions for instruction
   switch (cond) {
@@ -72,7 +72,7 @@ bool checkCond(arm state, word instruction) {
   }
 }
 
-void branch(arm state, word instruction) {
+void branch(arm *state, word instruction) {
   // no execution if condition code does not match
   if (!checkCond(state, instruction))
     return;
@@ -82,8 +82,8 @@ void branch(arm state, word instruction) {
   int signBit = offset & BRANCH_SIGN_BIT;
 
   // Shift, sign extension and addition of offset onto current address
-  state.registers[PC] +=
-      (offset << PIPELINE_SHIFT) |
+  state->registers[PC] +=
+      (offset << CURRENT_INSTRUCTION_SHIFT) |
       (signBit ? NEGATIVE_SIGN_EXTEND : POSITIVE_SIGN_EXTEND);
 }
 
