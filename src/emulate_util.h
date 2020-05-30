fc8d9bd0d63d76d471af6b4ea908d0b83eff0c82
Alex R <arr19@ic.ac.uk>
2020-05-30 22:34:13 +0100

    Added enums from emulate_util.c

--- src/emulate_util.h
+++ src/emulate_util.h
@@ -9,10 +9,7 @@ typedef uint32_t word;
 typedef uint8_t byte;
 typedef unsigned int uint;
 
-// ARM instruction set
-typedef enum { DPI, MULT, BR, SDTI, IGNR } InstructionSet;
-
-// TODO: consider changing name (and type of carryOut)
+// TODO: consider changing the type of carryOut
 typedef struct {
   word result;
   word carryOut;
@@ -32,6 +29,19 @@ typedef struct {
   word fetched;
   tuple_instruction decoded;
 } arm;
+
+/*registers 0-12 will be used by their value so for reg0 we can just use 0
+but these will make it easier to address in memory*/
+enum Register { PC = 15, CPSR = 16 };
+// opcode mnemonics
+enum Opcode { AND, EOR, SUB, RSB, ADD, TST = 8, TEQ, CMP, ORR = 12, MOV };
+// condition suffixes
+enum Cond { EQ, NE, GE = 10, LT, GT, LE, AL };
+// shift types
+enum Shift { LSL, LSR, ASR, ROR };
+// ARM instruction set
+typedef enum { DPI, MULT, BR, SDTI, IGNR } InstructionSet;
+
 
 extern void check_ptr(const void *ptr, const char *error_msg);
 
