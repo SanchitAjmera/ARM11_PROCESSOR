6aeecffb590bdf735c6845aa41ab0863257d09de
Alex R <arr19@ic.ac.uk>
2020-06-02 17:05:33 +0100

    Removed instuction var from each intruction type struct

--- src/emulate_util.c
+++ src/emulate_util.c
@@ -196,7 +196,7 @@ void executeDPI(arm_t *state, dp_t *decoded) {
     assert(false);
   }
   // if s (bit 20) is set then the CPSR flags should be updated
-  if (UPDATE_CPSR(decoded->instruction)) {
+  if (decoded->s) {
     setCPSR(state, result, carryOut);
   }
   free(shiftedOp2);
@@ -341,11 +341,12 @@ void fetch(arm_t *state) {
 
 dp_t *decodeDPI(arm_t *state, word instruction) {
   dp_t *decoded = malloc(sizeof(dp_t));
-  decoded->instruction = instruction;
   // decoded of code
   decoded->i = (instruction & DPI_I_MASK) >> DPI_I_SHIFT;
   // instruction to execute
   decoded->opcode = (instruction & DPI_OPCODE_MASK) >> DPI_OPCODE_SHIFT;
+  // if s (bit 20) is set then the CPSR flags should be updated
+  decoded->s = UPDATE_CPSR(instruction);
   // op1 is always the contents of register Rn
   decoded->rn = (instruction & DPI_RN_MASK) >> DPI_RN_SHIFT;
   // destination register
@@ -359,7 +360,6 @@ dp_t *decodeDPI(arm_t *state, word instruction) {
 
 multiply_t *decodeMultiply(arm_t *state, word instruction) {
   multiply_t *decoded = malloc(sizeof(multiply_t));
-  decoded->instruction = instruction;
   decoded->a = instruction & ACCUMULATE_FLAG;
   decoded->s = UPDATE_CPSR(instruction);
   decoded->destination = (instruction & MULT_RDEST_MASK) >> MULT_RDEST_SHIFT;
@@ -371,7 +371,6 @@ multiply_t *decodeMultiply(arm_t *state, word instruction) {
 
 sdt_t *decodeSDTI(arm_t *state, word instruction) {
   sdt_t *decoded = malloc(sizeof(sdt_t));
-  decoded->instruction = instruction;
   decoded->i = (instruction & SDTI_I_MASK) >> SDTI_I_SHIFT;
   decoded->p = (instruction & SDTI_P_MASK) >> SDTI_P_SHIFT;
   decoded->u = (instruction & SDTI_U_MASK) >> SDTI_U_SHIFT;
@@ -384,7 +383,6 @@ sdt_t *decodeSDTI(arm_t *state, word instruction) {
 
 branch_t *decodeBranch(arm_t *state, word instruction) {
   branch_t *decoded = malloc(sizeof(branch_t));
-  decoded->instruction = instruction;
   decoded->offset = instruction & BRANCH_OFFSET_MASK;
   return decoded;
 }
