5b79ed5e2e34870fe2f978f9d62cb3a955140b1d
ms1419 <ms1419@imperial.ac.uk>
2020-06-04 19:56:58 +0100

    added logic in scanFile() that adds all found labels and expressions to the symbol table

--- src/assemble_util.c
+++ src/assemble_util.c
@@ -68,12 +68,18 @@ file_lines *scanFile(FILE *armFile, symbol_table *symbolTable) {
   /* Scan file for labels and expressions */
   char line[LINE_CHAR_LIM];
   while (fgets(line, LINE_CHAR_LIM, armFile) != NULL) {
-    linesRead++;
     addLine(fileLines, strtok(line, "\n")); // Adds line stripped of \n
+
+    /* iterate through chars in line */
     for (int i = 0; i < strlen(line); i++) {
       if (line[i] == ':') { // Line is a label
         char *label = strtok(line, "\n");
-        printf("label %s\n", label);
+        // Make a new symbol for this label
+        symbol labelSymbol = {NULL, LABEL, .body.address = linesRead * 4};
+        labelSymbol.name = malloc(sizeof(char) * strlen(label));
+        strcpy(labelSymbol.name, label);
+
+        addSymbol(symbolTable, labelSymbol);
         break; // Line contains only one label
         /* TODO: extract label, calculate address, and store in symbol table */
       } else if (line[i] == '#') { // Line contains an expression
@@ -83,6 +89,20 @@ file_lines *scanFile(FILE *armFile, symbol_table *symbolTable) {
         break; // Each line contains up to one expression. Remove if incorrect
       }
     }
+    linesRead++;
+  }
+
+  /* Now that we have determined the number of lines in the ARM file,
+     we can add all expressions to the symbol table */
+  for (int i = 0; i < expressions->lineCount; i++) {
+    char *expr = expressions->lines[i];
+    // Make a new symbol for this expression
+    word address = (linesRead + i) * 4;
+    symbol exprSymbol = {NULL, LABEL, .body.address = address};
+    exprSymbol.name = malloc(sizeof(char) * strlen(expr));
+    strcpy(exprSymbol.name, expr);
+
+    addSymbol(symbolTable, exprSymbol);
   }
 
   addLines(fileLines, expressions->lines, expressions->lineCount);
