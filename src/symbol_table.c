67ca25a75dcb128969ec9bbfcf23b142105e5d0c
ms1419 <ms1419@imperial.ac.uk>
2020-06-04 16:54:59 +0100

    fixed resizing of the symbol table

--- src/symbol_table.c
+++ src/symbol_table.c
@@ -6,8 +6,12 @@
 
 #define INIT_S_TABLE_SIZE 32
 
+/* Takes in a pointer to an uninitialised symbol table and initialises it */
 void initSymbolTable(symbol_table *s) {
-  s->symbols = malloc(INIT_S_TABLE_SIZE * sizeof(symbol));
+  assert(s != NULL);
+  symbol *symbols = malloc(INIT_S_TABLE_SIZE * sizeof(symbol));
+  assert(symbols != NULL);
+  s->symbols = symbols;
   s->max_entries = INIT_S_TABLE_SIZE;
   s->entry_count = 0;
 }
@@ -26,14 +30,16 @@ symbol *getSymbol(const symbol_table *s, const char *name) {
 }
 
 void addSymbol(symbol_table *s, symbol entry) {
+  assert(s != NULL);
   if (getSymbol(s, entry.name) != NULL) { // Label already defined
     return;
   }
 
   if (s->entry_count == s->max_entries) {
     s->max_entries *= 2;
-    if (!realloc(s->symbols, s->max_entries))
+    if ((s->symbols = realloc(s->symbols, s->max_entries)) == NULL) {
       printf("error!");
+    }
   }
   s->symbols[s->entry_count] = entry;
   s->entry_count += 1;
