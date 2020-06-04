e1a59e8f41bebb734bef0cbc842ac0cde0019bb8
Luqman <lml19@imperial.ac.uk>
2020-06-04 17:10:07 +0100

    Extracted current address from struct

--- src/assemble_util.c
+++ src/assemble_util.c
@@ -86,14 +86,14 @@ word getCondition(const char *condition) {
   }
   return ALWAYS;
 }
-// to complete
+// to complete currentAddress
 word assembleBranch(symbol_table *symbolTable, instruction *input) {
   // need to change condition into an enum to cover all conds
   word cond =
       strlen(input->opcode) == 1 ? ALWAYS : getCondition(++(input->opcode));
 
   // need to find out how to obtain current address
-  word currentAddress = 0;
+  word currentAddress = input->currentAddress;
 
   char *target = input->fields[0];
   word targetAddress = (target[0] == '#')
