5694355d71c905594394d068a14457da7da4654d
ms1419 <ms1419@imperial.ac.uk>
2020-06-10 15:26:50 +0100

    added util file which holds general functions

---
+++ src/common/util.c
@@ -0,0 +1,24 @@
+#include "util.h"
+#include <stdlib.h>
+#include <string.h>
+
+/* Takes in a string and returns a pointer to that string on heap */
+char *strptr(char *in) {
+  char *out = malloc(sizeof(char) * (strlen(in)) + 1);
+  strcpy(in, out);
+  return out;
+}
+
+int validatePtr(const void *ptr, const char *error_msg) {
+  if (ptr == NULL) {
+    printf("Error: %s\n", error_msg);
+    return ERR_FAIL;
+  }
+  return ERR_PASS;
+}
+
+void errorExit(Error error) {
+  if (error == ERR_FAIL) {
+    exit(EXIT_FAILURE);
+  }
+}
