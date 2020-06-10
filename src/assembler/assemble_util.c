41cf09a6d9308b329fe811e47b3c2dfb1e03960e
ms1419 <ms1419@imperial.ac.uk>
2020-06-10 22:48:43 +0100

    removed error in merge

--- src/assembler/assemble_util.c
+++ src/assembler/assemble_util.c
@@ -1,15 +1,9 @@
 #include "assemble_util.h"
-<<<<<<< HEAD
-#include "include/constants.h"
-#include "include/file_lines.h"
-#include <assert.h>
-=======
 #include "../common/constants.h"
 #include "../common/util.h"
 #include "file_lines.h"
 #include <assert.h>
 #include <stdbool.h>
->>>>>>> assembler-tokens
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,7 +11,6 @@
 #define LINE_CHAR_LIM 512
 #define WORD_SIZE_BYTES 4
 
-<<<<<<< HEAD
 // Enum for different types of addresses in SDTI assembly instructions
 typedef enum SDTIOperation {
   NUMERIC_CONST,
@@ -26,22 +19,6 @@ typedef enum SDTIOperation {
   POST_RN_EXP
 } SDTIOperation;
 
-/* Scans a file adding labels to the symbol table,
-    as well as expressions. Returns an array of strings that
-    represent each line, stripped of the newline \n,
-    and stores expressions in their string form at the end of the array. */
-file_lines *scanFile(FILE *armFile, symbol_table *symbolTable) {
-  assert(armFile != NULL && symbolTable != NULL);
-
-  /* Build a new file_lines struct which stores each line in the
-    ARM file in an array, and any expressions gathered at the end */
-  file_lines *fileLines = malloc(sizeof(file_lines));
-  initFileLines(fileLines);
-
-  /* Will be used to store expressions found during the scan */
-  file_lines *expressions = malloc(sizeof(file_lines));
-  initFileLines(expressions);
-=======
 /* Performs the first scan on a file adding labels to the symbol table,
     as well as expressions. Returns an array of strings that
     represent each line, stripped of the newline \n,
@@ -51,30 +28,10 @@ void scanFile(FILE *armFile, symbol_table *symbolTable, file_lines *output) {
 
   /* Will be used to store expressions found during the scan */
   file_lines *expressions = newFileLines();
->>>>>>> assembler-tokens
 
   /* Scan file for labels and expressions */
   char line[LINE_CHAR_LIM];
   while (fgets(line, LINE_CHAR_LIM, armFile) != NULL) {
-<<<<<<< HEAD
-    addLine(fileLines, strtok(line, "\n")); // Adds line stripped of \n
-
-    /* iterate through chars in line */
-    for (int i = 0; i < strlen(line); i++) {
-      if (line[i] == ':') { // Line is a label
-        char *label = strtok(line, "\n");
-        // Make a new symbol for this label
-        symbol labelSymbol = {NULL, LABEL,
-                              .body.address =
-                                  fileLines->lineCount * WORD_SIZE_BYTES};
-        labelSymbol.name = malloc(sizeof(char) * strlen(label));
-        strcpy(labelSymbol.name, label);
-
-        addSymbol(symbolTable, labelSymbol);
-        break; // Line contains only one label
-        /* TODO: extract label, calculate address, and store in symbol table */
-      } else if (line[i] == '#') { // Line contains an expression
-=======
     /* iterate through chars in line */
     bool isLabel = false;
     for (int i = 0; i < strlen(line); i++) {
@@ -87,33 +44,62 @@ void scanFile(FILE *armFile, symbol_table *symbolTable, file_lines *output) {
         isLabel = true;
         break;                     // Line contains only one label
       } else if (line[i] == '=') { // Line contains an =0x expression
->>>>>>> assembler-tokens
         char *expression =
             strtok(line + i - 1, " ],\n"); // Pulls expression out
         addLine(expressions, expression);
         break; // Each line contains up to one expression. Remove if incorrect
       }
     }
-<<<<<<< HEAD
+    if (isLabel == false) {
+      addLine(output, strtok(line, "\n")); // Adds line stripped of \n
+    }
   }
 
-  /* Now that we have added all lines to fileLines, we can add all expressions
+  /* Now that we have added all lines to output, we can add all expressions
      to the symbol table and calculate their address using lineCount */
   for (int i = 0; i < expressions->lineCount; i++) {
     char *expr = expressions->lines[i];
-    word address = (fileLines->lineCount + i) * WORD_SIZE_BYTES;
-    // Make a new symbol for this expression
-    symbol exprSymbol = {NULL, LABEL, .body.address = address};
-    exprSymbol.name = malloc(sizeof(char) * strlen(expr));
-    strcpy(exprSymbol.name, expr);
-
+    word address = (output->lineCount + i) * WORD_SIZE_BYTES;
+    symbol exprSymbol = {strptr(expr), LABEL, .body.address = address};
     addSymbol(symbolTable, exprSymbol);
   }
 
-  addLines(fileLines, expressions->lines, expressions->lineCount);
+  addLines(output, expressions->lines, expressions->lineCount);
 
   freeFileLines(expressions);
-  return fileLines;
+}
+
+/*  Performs the second pass on fileLines */
+void parseLines(file_lines *in, symbol_table *symbolTable, FILE *out) {
+  for (int i = 0; i < in->lineCount; i++) {
+    char *line = in->lines[i];
+    char *rest = NULL;
+
+    char *fields[5];
+    int fieldCount = 0;
+    char *token = strtok_r(line, " ,", &rest);
+    while (token != NULL) {
+      fields[fieldCount++] = token;
+      if (rest[0] == '[') { // If the next token starts with a [
+        token = strtok_r(rest, "]", &rest);
+      } else {
+        token = strtok_r(NULL, " ,", &rest);
+      }
+    }
+
+    instruction instr = {fields[0], fields + 1, fieldCount - 1,
+                         i * WORD_SIZE_BYTES};
+    symbol *instrSymbol = getSymbol(symbolTable, instr.opcode);
+    assert(instrSymbol != NULL);
+    assert(instrSymbol->type == INSTR);
+    word binLine = instrSymbol->body.assembleFunc(symbolTable, instr);
+    printf("output: %x\n", binLine);
+
+    for (int j = 0; j < fieldCount; j++) {
+      printf("token: %s\n", fields[j]);
+    }
+    printf("\n");
+  }
 }
 
 // removes first character and returns integer from string
@@ -378,56 +364,3 @@ word assembleBranch(symbol_table *symbolTable, instruction *input) {
     // returning constructed instruction
     return ALWAYS | SDTI_HARDCODE | i | p | u | l | Rn | Rd | offset;
   }
-=======
-    if (isLabel == false) {
-      addLine(output, strtok(line, "\n")); // Adds line stripped of \n
-    }
-  }
-
-  /* Now that we have added all lines to output, we can add all expressions
-     to the symbol table and calculate their address using lineCount */
-  for (int i = 0; i < expressions->lineCount; i++) {
-    char *expr = expressions->lines[i];
-    word address = (output->lineCount + i) * WORD_SIZE_BYTES;
-    symbol exprSymbol = {strptr(expr), LABEL, .body.address = address};
-    addSymbol(symbolTable, exprSymbol);
-  }
-
-  addLines(output, expressions->lines, expressions->lineCount);
-
-  freeFileLines(expressions);
-}
-
-/*  Performs the second pass on fileLines */
-void parseLines(file_lines *in, symbol_table *symbolTable, FILE *out) {
-  for (int i = 0; i < in->lineCount; i++) {
-    char *line = in->lines[i];
-    char *rest = NULL;
-
-    char *fields[5];
-    int fieldCount = 0;
-    char *token = strtok_r(line, " ,", &rest);
-    while (token != NULL) {
-      fields[fieldCount++] = token;
-      if (rest[0] == '[') { // If the next token starts with a [
-        token = strtok_r(rest, "]", &rest);
-      } else {
-        token = strtok_r(NULL, " ,", &rest);
-      }
-    }
-
-    instruction instr = {fields[0], fields + 1, fieldCount - 1,
-                         i * WORD_SIZE_BYTES};
-    symbol *instrSymbol = getSymbol(symbolTable, instr.opcode);
-    assert(instrSymbol != NULL);
-    assert(instrSymbol->type == INSTR);
-    word binLine = instrSymbol->body.assembleFunc(symbolTable, instr);
-    printf("output: %x\n", binLine);
-
-    for (int j = 0; j < fieldCount; j++) {
-      printf("token: %s\n", fields[j]);
-    }
-    printf("\n");
-  }
-}
->>>>>>> assembler-tokens
