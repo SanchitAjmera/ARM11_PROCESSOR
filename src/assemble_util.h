14d324ccf7f117202870f8c4c34aeb9926d9293d
Alex R <arr19@ic.ac.uk>
2020-06-10 12:16:06 +0100

    Added constants for assembleDPI

--- src/assemble_util.h
+++ src/assemble_util.h
@@ -10,6 +10,11 @@ extern file_lines *scanFile(FILE *armFile, symbol_table *symbolTable);
 // used for assemble dpi
 #define IS_IMMEDIATE(op) (op[0] == '#')
 #define DPI_COND (14 << 28) // 1110 (al)
+#define DPI_I_SHIFT 25
+#define DPI_OPCODE_SHIFT 21
+#define DPI_RN_SHIFT 16
+#define DPI_RD_SHIFT 12
+#define INSTRUCTION_S_MASK 20
 // opcode mnemonics
 enum Opcode { AND, EOR, SUB, RSB, ADD, TST = 8, TEQ, CMP, ORR = 12, MOV };
 // shift types
