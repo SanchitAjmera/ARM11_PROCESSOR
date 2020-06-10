e73e94ea840ac203d8b42e44dd7c47d2bf51f0bf
Alex R <arr19@ic.ac.uk>
2020-06-10 18:14:03 +0100

    Added macros for lookup and max representation

--- src/assemble_util.h
+++ src/assemble_util.h
@@ -9,7 +9,13 @@ extern file_lines *scanFile(FILE *armFile, symbol_table *symbolTable);
 
 // used for assemble dpi
 #define IS_IMMEDIATE(op) (op[0] == '#')
+// TODO: MAX_NUM (pow(2, WORD_SIZE) - 1)
+#define MAX_NUM (4294967295)
+#define OVERFLOW(num) (num > MAX_NUM)
+#define MAX_BYTE (0xFF)
 #define HEX_BASE (16)
+#define LOOKUP_FAILURE (-1)
+#define LOOKUP_FAILED(num) (num == LOOKUP_FAILURE)
 #define DPI_COND (14 << 28) // 1110 (al)
 #define DPI_I_SHIFT 25
 #define DPI_OPCODE_SHIFT 21
@@ -20,7 +26,7 @@ extern file_lines *scanFile(FILE *armFile, symbol_table *symbolTable);
 enum Opcode { AND, EOR, SUB, RSB, ADD, TST = 8, TEQ, CMP, ORR = 12, MOV };
 // shift types
 enum Shift { LSL, LSR, ASR, ROR };
-// generic (key, value) struct for lookups
+// generic (string, enum) struct for lookups
 typedef struct {
   char *key;
   int value;
