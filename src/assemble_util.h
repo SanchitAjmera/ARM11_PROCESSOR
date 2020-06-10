00e1ed9b3def1d97f14ab95664dcc66df60689b2
Alex R <arr19@ic.ac.uk>
2020-06-10 14:59:51 +0100

    Renamed INSTRUCTION_S_MASK to DPI_S_SHIFT

--- src/assemble_util.h
+++ src/assemble_util.h
@@ -14,7 +14,7 @@ extern file_lines *scanFile(FILE *armFile, symbol_table *symbolTable);
 #define DPI_OPCODE_SHIFT 21
 #define DPI_RN_SHIFT 16
 #define DPI_RD_SHIFT 12
-#define INSTRUCTION_S_MASK 20
+#define DPI_S_SHIFT 20
 // opcode mnemonics
 enum Opcode { AND, EOR, SUB, RSB, ADD, TST = 8, TEQ, CMP, ORR = 12, MOV };
 // shift types
