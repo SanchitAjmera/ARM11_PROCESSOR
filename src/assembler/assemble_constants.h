f52c6f6a9bf78bb45d0e205551b0dbd897a838d4
Alex R <arr19@ic.ac.uk>
2020-06-11 23:55:31 +0100

    Added brackets to all macros (everyone)

--- src/assembler/assemble_constants.h
+++ src/assembler/assemble_constants.h
@@ -8,31 +8,30 @@
 #define OVERFLOW(num) (num > MAX_NUM)
 #define LOOKUP_FAILURE (-1)
 #define LOOKUP_FAILED(num) (num == LOOKUP_FAILURE)
-#define LINE_CHAR_LIM 512
+#define LINE_CHAR_LIM (512)
 
 // constants for File Lines
-#define INIT_FILE_LINES_SIZE 16
+#define INIT_FILE_LINES_SIZE (16)
 
 // constants for Symbol Table
-#define INIT_S_TABLE_SIZE 32
+#define INIT_S_TABLE_SIZE (32)
 
 // constants for Data Processing Instructions
 #define MAX_BYTE (0xFF)
 #define HEX_BASE (16)
-#define DPI_S_SHIFT 20
+#define DPI_S_SHIFT (20)
 #define SHIFT_TABLE_SIZE (4)
 #define OPCODE_TABLE_SIZE (12)
 
 // constants for Single Data Transfer Instructions
 #define SDTI_HARDCODE (1 << 26)
-#define SDTI_EXP_BOUND 0xFF
+#define SDTI_EXP_BOUND (0xFF)
 
 // constants for Multiply Instructions
 #define MULT_HARDCODE (0x90)
 
 // constants for Branch Instructions
-#define BRANCH_HARDCODE 0x0A000000
-
+#define BRANCH_HARDCODE (0x0A000000)
 #define COND_TABLE_SIZE (7)
 
 #endif
--- src/common/constants.h
+++ src/common/constants.h
@@ -2,40 +2,40 @@
 #define CONSTANTS_H
 
 // general constants
-#define MEMORY_CAPACITY 65536
-#define NUM_REGISTERS 17
-#define WORD_SIZE 32
-#define ADDRESS_SIZE 4
-#define WORD_SIZE_BYTES 4
-#define BYTE 8
-#define COND_SHIFT 28
+#define MEMORY_CAPACITY (65536)
+#define NUM_REGISTERS (17)
+#define WORD_SIZE (32)
+#define ADDRESS_SIZE (4)
+#define WORD_SIZE_BYTES (4)
+#define BYTE (8)
+#define COND_SHIFT (28)
 #define ALWAYS (14 << COND_SHIFT)
 
 // constants for Data Processing Instructions
-#define ROTATION_FACTOR 2
-#define DPI_I_SHIFT 25
-#define DPI_OPCODE_SHIFT 21
-#define DPI_RN_SHIFT 16
-#define DPI_RD_SHIFT 12
+#define ROTATION_FACTOR (2)
+#define DPI_I_SHIFT (25)
+#define DPI_OPCODE_SHIFT (21)
+#define DPI_RN_SHIFT (16)
+#define DPI_RD_SHIFT (12)
 
 // constants for Single Data Transfer Instructions
-#define SDTI_I_SHIFT 25
-#define SDTI_P_SHIFT 24
-#define SDTI_U_SHIFT 23
-#define SDTI_L_SHIFT 20
-#define SDTI_RN_SHIFT 16
-#define SDTI_RD_SHIFT 12
+#define SDTI_I_SHIFT (25)
+#define SDTI_P_SHIFT (24)
+#define SDTI_U_SHIFT (23)
+#define SDTI_L_SHIFT (20)
+#define SDTI_RN_SHIFT (16)
+#define SDTI_RD_SHIFT (12)
 
 // constants for Multiply Instructions
-#define MULT_RDEST_MASK 0x000F0000
-#define MULT_REG_S_MASK 0x00000F00
-#define MULT_REG_N_MASK 0x0000F000
-#define MULT_REG_S_SHIFT 8
-#define MULT_REG_N_SHIFT 12
-#define MULT_RDEST_SHIFT 16
+#define MULT_RDEST_MASK (0x000F0000)
+#define MULT_REG_S_MASK (0x00000F00)
+#define MULT_REG_N_MASK (0x0000F000)
+#define MULT_REG_S_SHIFT (8)
+#define MULT_REG_N_SHIFT (12)
+#define MULT_RDEST_SHIFT (16)
 #define ACCUMULATE_FLAG (1 << 21)
 
 // constants for Branch Instructions
-#define BRANCH_OFFSET_MASK 0x00FFFFFF
+#define BRANCH_OFFSET_MASK (0x00FFFFFF)
 
 #endif
