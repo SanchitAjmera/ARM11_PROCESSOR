e3651fe0778d12aea519e4f28e8e979f80c57625
Alex R <arr19@ic.ac.uk>
2020-06-11 23:45:19 +0100

    Removed redundant enums

--- src/assembler/assemble_util.h
+++ src/assembler/assemble_util.h
@@ -6,33 +6,6 @@
 #include "symbol_table.h"
 #include <stdio.h>
 
-#define LINE_CHAR_LIM 512
-#define WORD_SIZE_BYTES 4
-#define ROTATION_FACTOR 2
-
-/*registers 0-12 will be used by their value so for reg0 we can just use 0
-but these will make it easier to address in memory*/
-enum Register { PC = 15, CPSR = 16 };
-// opcode mnemonics
-enum Opcode {
-  AND,
-  EOR,
-  SUB,
-  RSB,
-  ADD,
-  TST = 8,
-  TEQ,
-  CMP,
-  ORR = 12,
-  MOV,
-  ANDEQ,
-  LSL_SPECIAL,
-};
-// condition suffixes
-enum Cond { EQ, NE, GE = 10, LT, GT, LE, AL };
-// shift types
-enum Shift { LSL, LSR, ASR, ROR };
-
 // generic (string, enum) struct for lookups
 typedef struct {
   char *key;
@@ -40,12 +13,7 @@ typedef struct {
 } pair_t;
 
 // Enum for different types of addresses in SDTI assembly instructions
-typedef enum SDTIOperation {
-  NUMERIC_CONST,
-  PRE_RN,
-  PRE_RN_EXP,
-  POST_RN_EXP
-} SDTIOperation;
+typedef enum { NUMERIC_CONST, PRE_RN, PRE_RN_EXP, POST_RN_EXP } SDTIOperation;
 
 // To be used with Lookup for string to enum conversion in getCondition;
 static const pair_t condTable[] = {{"eq", EQ}, {"ne", NE}, {"ge", GE},
@@ -59,6 +27,7 @@ static const pair_t opcodeTable[] = {
     {"add", ADD}, {"tst", TST}, {"teq", TEQ},     {"cmp", CMP},
     {"orr", ORR}, {"mov", MOV}, {"andeq", ANDEQ}, {"lsl", LSL_SPECIAL}};
 
+// assemble functions for all instruction types
 extern word assembleDPI(symbol_table *symbolTable, instruction input);
 extern word assembleSDTI(symbol_table *symbolTable, instruction input);
 extern word assembleMultiply(symbol_table *symbolTable, instruction input);
--- src/emulator/emulate_util.h
+++ src/emulator/emulate_util.h
@@ -7,15 +7,6 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-/*registers 0-12 will be used by their value so for reg0 we can just use 0
-but these will make it easier to address in memory*/
-enum Register { PC = 15, CPSR = 16 };
-// opcode mnemonics
-enum Opcode { AND, EOR, SUB, RSB, ADD, TST = 8, TEQ, CMP, ORR = 12, MOV };
-// condition suffixes
-enum Cond { EQ, NE, GE = 10, LT, GT, LE, AL };
-// shift types
-enum Shift { LSL, LSR, ASR, ROR };
 // ARM instruction set
 typedef enum { DPI, MULT, BR, SDTI, IGNR } InstructionType;
 
@@ -90,7 +81,6 @@ typedef struct {
  * memory and register
  * pointers on heap, where memory is of size MEM_LIMIT bytes */
 extern void initArm(arm_t *state, const char *fname);
-
 extern word getWord(byte *start_addr, bool isBigEndian);
 extern void fetch(arm_t *state);
 extern void decode(arm_t *state, decoded_t *decoded);
