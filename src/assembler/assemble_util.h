57ca3cb07f4a03a8a568166fe81abb05290c9ead
Alex R <arr19@ic.ac.uk>
2020-06-11 13:02:33 +0100

    Moved macros from assemble_util.h to assemble_constants.h

--- src/assembler/assemble_util.h
+++ src/assembler/assemble_util.h
@@ -26,25 +26,6 @@ typedef enum SDTIOperation {
 // Will be removed from this file in future (accessible from constants)
 enum Cond { EQ, NE, GE = 10, LT, GT, LE, AL };
 
-// used for assemble dpi
-#define IS_IMMEDIATE(op) (op[0] == '#')
-// TODO: MAX_NUM (pow(2, WORD_SIZE) - 1)
-#define MAX_NUM (4294967295)
-#define OVERFLOW(num) (num > MAX_NUM)
-#define MAX_BYTE (0xFF)
-#define HEX_BASE (16)
-#define LOOKUP_FAILURE (-1)
-#define LOOKUP_FAILED(num) (num == LOOKUP_FAILURE)
-// 1110 (al)
-#define DPI_COND (14 << 28)
-#define DPI_I_SHIFT 25
-#define DPI_OPCODE_SHIFT 21
-#define DPI_RN_SHIFT 16
-#define DPI_RD_SHIFT 12
-#define DPI_S_SHIFT 20
-#define SHIFT_TABLE_SIZE (4)
-#define OPCODE_TABLE_SIZE (10)
-#define COND_TABLE_SIZE (7)
 // opcode mnemonics
 enum Opcode { AND, EOR, SUB, RSB, ADD, TST = 8, TEQ, CMP, ORR = 12, MOV };
 // shift types
@@ -62,10 +43,10 @@ static const pair_t opcodeTable[] = {
     {"AND", AND}, {"EOR", EOR}, {"SUB", SUB}, {"RSB", RSB}, {"ADD", ADD},
     {"TST", TST}, {"TEQ", TEQ}, {"CMP", CMP}, {"ORR", ORR}, {"MOV", MOV}};
 
-extern word assembleDPI(symbol_table *symbolTable, instruction *input);
-extern word assembleSDTI(symbol_table *symbolTable, instruction *input);
-extern word assembleMultiply(symbol_table *symbolTable, instruction *input);
-extern word assembleBranch(symbol_table *symbolTable, instruction *input);
+extern word assembleDPI(symbol_table *symbolTable, instruction input);
+extern word assembleSDTI(symbol_table *symbolTable, instruction input);
+extern word assembleMultiply(symbol_table *symbolTable, instruction input);
+extern word assembleBranch(symbol_table *symbolTable, instruction input);
 
 extern void scanFile(FILE *armFile, symbol_table *symbolTable,
                      file_lines *output);
