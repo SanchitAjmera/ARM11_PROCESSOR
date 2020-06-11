f740a059387b05d14291b046910606aa339ca3de
Alex R <arr19@ic.ac.uk>
2020-06-11 23:38:34 +0100

    Added commonly used enums

--- src/common/util.h
+++ src/common/util.h
@@ -4,6 +4,28 @@
 #include <stdint.h>
 
 typedef enum { ERR_PASS = 0, ERR_FAIL = 1 } Error;
+/*registers 0-12 will be used by their value so for reg0 we can just use 0
+but these will make it easier to address in memory*/
+enum Register { PC = 15, CPSR = 16 };
+// opcode mnemonics
+enum Opcode {
+  AND,
+  EOR,
+  SUB,
+  RSB,
+  ADD,
+  TST = 8,
+  TEQ,
+  CMP,
+  ORR = 12,
+  MOV,
+  ANDEQ,
+  LSL_SPECIAL,
+};
+// condition suffixes
+enum Cond { EQ, NE, GE = 10, LT, GT, LE, AL };
+// shift types
+enum Shift { LSL, LSR, ASR, ROR };
 
 typedef uint32_t word;
 typedef uint8_t byte;
