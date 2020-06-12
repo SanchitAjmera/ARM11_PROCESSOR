eee757cec16020a7fe320888e7d8ea766b7d86f6
sa3119 <sa3119@ic.ac.uk>
2020-06-12 20:43:25 +0100

    minor changes to contents of state struct

--- extension/util.h
+++ extension/util.h
@@ -5,11 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_ROOM_HISTORY
+
 // enum for items stored by person in inventory
 typedef enum { ITEM1, ITEM2, ETC } item;
 
 // enum for room
-typedef enum { LOBBY, LAB, LECTURE_HALL, FUSION } room;
+typedef enum { LOBBY, LAB, LECTURE_HALL, FUSION, HARRODS } room;
 
 // enum for character type
 typedef enum { BATMAN, TONY, UTA } character;
@@ -19,8 +21,9 @@ typedef struct {
   int cash;
   int health;
   room current_room;
+  room room_history[MAX_ROOM_HISTORY];
   struct {
     char *username;
     character character;
   } profile;
-} person;
+} state;
