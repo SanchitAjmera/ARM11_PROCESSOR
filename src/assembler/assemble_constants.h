35b4d87a24115efdfc805460a6dca90298ea9cfe
Alex R <arr19@ic.ac.uk>
2020-06-12 12:05:44 +0100

    Removed OVERFLOW()

--- src/assembler/assemble_constants.h
+++ src/assembler/assemble_constants.h
@@ -5,11 +5,10 @@
 #define IS_IMMEDIATE(op) (op[0] == '#' || op[0] == '=')
 #define REMOVE_FIRST_CHAR(string) (++string)
 #define MAX_NUM (4294967295)
-// TODO: MAX_NUM (pow(2, WORD_SIZE) - 1)
-#define OVERFLOW(num) (num > MAX_NUM)
 #define LOOKUP_FAILURE (-1)
 #define LOOKUP_FAILED(num) (num == LOOKUP_FAILURE)
 #define LINE_CHAR_LIM (512)
+#define PREDEFINED_SYMBOLS_COUNT 23
 
 // constants for File Lines
 #define INIT_FILE_LINES_SIZE (16)
