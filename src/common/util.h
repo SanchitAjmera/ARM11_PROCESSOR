3f348020b98ad03de78c1492c21e6a5347be2d9f
Alex R <arr19@ic.ac.uk>
2020-06-12 13:05:02 +0100

    Added all enums for Opcodes (Luqman)

--- src/common/util.h
+++ src/common/util.h
@@ -3,11 +3,15 @@
 
 #include <stdint.h>
 
-typedef enum { ERR_PASS = 0, ERR_FAIL = 1 } Error;
+typedef uint32_t word;
+typedef uint8_t byte;
+typedef unsigned int uint;
+
+typedef enum { ERROR_SUCC = 0, ERR_FAIL = 1 } Error;
 /*registers 0-12 will be used by their value so for reg0 we can just use 0
 but these will make it easier to address in memory*/
 typedef enum { PC = 15, CPSR = 16 } Register;
-// opcode mnemonics
+// opcode mnemonics for DPI
 typedef enum {
   AND,
   EOR,
@@ -18,18 +22,20 @@ typedef enum {
   TEQ,
   CMP,
   ORR = 12,
-  MOV,
-  ANDEQ,
-  LSL_SPECIAL,
-} Opcode;
+  MOV
+} OpcodeDP;
+// opcode mnemonics for Multiply Instructions
+typedef enum { MUL, MLA } OpcodeMultiply;
+// opcode mnemonics for SDTI
+typedef enum { LDR, STR } OpcodeSDT;
+// opcode mnemonics for Branch Instructions
+typedef enum { BEQ, BNE, BGE, BLT, BGT, BLE, B } OpcodeBranch;
+// opcode mnemonics for Special Instructions
+typedef enum { LSL_SPECIAL, ANDEQ } OpcodeSpecial;
 // condition suffixes
 typedef enum { EQ, NE, GE = 10, LT, GT, LE, AL } Cond;
 // shift types
 typedef enum { LSL, LSR, ASR, ROR } Shift;
-
-typedef uint32_t word;
-typedef uint8_t byte;
-typedef unsigned int uint;
 
 extern char *strptr(char *in);
 extern Error validatePtr(const void *ptr, const char *errorMsg);
