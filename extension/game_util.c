f4b7c50004aa8ae0dc2bf6f9ff5209d94629aa0c
sa3119 <sa3119@ic.ac.uk>
2020-06-12 20:41:50 +0100

    added function bodies for possible functions

---
+++ extension/game_util.c
@@ -0,0 +1,28 @@
+#include "util.h"
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// shows player their inventory of items
+void view_inventory(){};
+
+// shows player their character and username
+void view_profile(){};
+
+// shows player their current room
+void current_room(){};
+
+// shows player room which they can visit
+// possibly a map (like the lego land map)
+void display_rooms(){};
+
+// quits game also saving game into some history file
+void quit(){};
+
+// displays introduction to player
+void introduction(){};
+
+int main(void) { return 1; }
