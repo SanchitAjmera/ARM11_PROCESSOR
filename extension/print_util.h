8d5d5a6d184a62671e43eeb536fbb520d533b5a8
sa3119 <sa3119@ic.ac.uk>
2020-06-14 06:18:58 +0100

    added further print graphics for kgk

--- extension/print_util.h
+++ extension/print_util.h
@@ -8,5 +8,8 @@ void printAdjacentRooms(room_t *room1);
 void printRoomDetails(room_t *room1);
 void printBuildingDetails(building_t *huxley);
 void printKonstantinos(void);
+void printKonstantinosMouthOpen(void);
+void printKonstantinosIndent(void);
+void printKonstantinosMouthOpenIndent(void);
 
 #endif
