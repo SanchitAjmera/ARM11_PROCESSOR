c85445623f9bf0121ba536dd8b2bd5dd8d9d2b77
Alex R <arr19@ic.ac.uk>
2020-06-14 14:38:24 +0100

    Added createBoss and freeBoss functions

--- extension/boss.c
+++ extension/boss.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// dummy function
+void validatePtr(void *pointer, const char *) {}
+
+// function to create and initialise a pointer to boss_t on the heap
+boss_t *createBoss() {
+  boss_t *boss = malloc(sizeof(*boss));
+  validatePtr(boss, "Not enough memory.");
+  boss->isPassive = false;
+  boss->teaching = malloc(sizeof(*boss->teaching));
+  validatePtr(boss->teaching, "Not enough memory.");
+}
+
+void freeBoss(boss_t *boss) {
+  free(boss->teaching);
+  free(boss->fighting);
+  free(boss);
+}
 
 // function to start the quiz on assembly code
 void quiz() {}
