9d5fa0529c7d3e51768fb3baf71d2cfb9cf97b78
Alex R <arr19@ic.ac.uk>
2020-06-14 17:00:28 +0100

    Added printAttack function

--- extension/battle.c
+++ extension/battle.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 // function to start the battle with the boss
-void battle(boss_t *boss) {
+void battle(boss_t *boss, player_t *player) {
   // PRE: boss->fighting has been initialised
   // TODO: turn based combat against the player
 }
@@ -19,11 +19,21 @@ bool playerWon(boss_t *boss, player_t *player) {
   return boss->fighting->health < player->health;
 }
 
+void printAttack(const char *name, int damage, const char *attackName) {
+  printf("%s used %s...\n...and dealt %d damage!\n", name, attackName, damage);
+}
+
 // deals damage to opponent
-void attack() {}
+void attackPlayer(boss_t *boss, player_t *player) {
+  int damage = boss->fighting->attack;
+  player->health -= damage;
+  printAttack(boss->name, damage, boss->fighting->attackName);
+}
 
 // player's turn in battle
-void playerTurn(boss_t *boss, player_t *player) {}
+void playerTurn(boss_t *boss, player_t *player) {
+  // TODO: let them choose a form of attack
+}
 
 // boss's turn in battle
 void bossTurn(boss_t *boss, player_t *player) {}
