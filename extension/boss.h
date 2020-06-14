a7718efc66a3cfd4cb4fc1002258a8d1485c846c
Alex R <arr19@ic.ac.uk>
2020-06-14 01:55:29 +0100

    Added initial structs

--- extension/boss.h
+++ extension/boss.h
@@ -1,4 +1,29 @@
 #ifndef BOSS_H
 #define BOSS_H
 
+#include <stdbool.h>
+
+typedef struct {
+  char **questions;
+  char **answers;
+} passive_t;
+
+typedef struct {
+  int attack;
+  int special;
+  char *attackName;
+  char *specialName;
+  int health;
+  int stamina;
+} aggressive_t;
+
+typedef struct {
+  char *name;
+  bool isPassive;
+  union {
+    passive_t *teaching;
+    aggressive_t *fighting;
+  }
+} boss_t;
+
 #endif
