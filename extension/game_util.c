0f9465f102711385c12feab6c1c94a81c454b124
sa3119 <sa3119@ic.ac.uk>
2020-06-14 18:40:37 +0100

    initialising rooms with room positions

--- extension/game_util.c
+++ extension/game_util.c
@@ -40,9 +40,10 @@ void connectRoom(room_t *first, room_t *second) {
 }
 
 // connects all room positions to center room position r5
-void connectRoomPositions(room_t *r1,room_t *r2,room_t *r3,room_t *r4,room_t *r5){
-  room_t *roomArray[ROOM_POSITION_NUMBER -1] = {r1,r2,r3,r4};
-  for(int i = 0 ; i< ROOM_POSITION_NUMBER -1; i++){
+void connectRoomPositions(room_t *r1, room_t *r2, room_t *r3, room_t *r4,
+                          room_t *r5) {
+  room_t *roomArray[ROOM_POSITION_NUMBER - 1] = {r1, r2, r3, r4};
+  for (int i = 0; i < ROOM_POSITION_NUMBER - 1; i++) {
     connectRoom(roomArray[i], r5);
   }
 }
@@ -72,19 +73,54 @@ building_t *initialiseBuilding() {
   huxley->start_room = malloc(sizeof(room_t));
   assert(huxley->start_room);
   // initialises all the rooms within the building
-  room_t *lobby = initialiseRoom(LOBBY, SOUTH);
-  room_t *lab = initialiseRoom(LAB, SOUTH);
-  room_t *fusion = initialiseRoom(FUSION, SOUTH);
-  room_t *lectureHall = initialiseRoom(LECTURE_HALL, SOUTH);
-  room_t *harrods = initialiseRoom(HARRODS, SOUTH);
+  room_t *lobbySouth = initialiseRoom(LOBBY, SOUTH);
+  room_t *lobbyEast = initialiseRoom(LOBBY, EAST);
+  room_t *lobbyWest = initialiseRoom(LOBBY, WEST);
+  room_t *lobbyNorth = initialiseRoom(LOBBY, NORTH);
+  room_t *lobbyCentre = initialiseRoom(LOBBY, CENTRE);
+
+  room_t *labSouth = initialiseRoom(LAB, SOUTH);
+  room_t *labEast = initialiseRoom(LAB, EAST);
+  room_t *labWest = initialiseRoom(LAB, WEST);
+  room_t *labNorth = initialiseRoom(LAB, NORTH);
+  room_t *labCentre = initialiseRoom(LAB, CENTRE);
+
+  room_t *fusionSouth = initialiseRoom(FUSION, SOUTH);
+  room_t *fusionEast = initialiseRoom(FUSION, EAST);
+  room_t *fusionWest = initialiseRoom(FUSION, WEST);
+  room_t *fusionNorth = initialiseRoom(FUSION, NORTH);
+  room_t *fusionCentre = initialiseRoom(FUSION, CENTRE);
+
+  room_t *lectureHallSouth = initialiseRoom(LECTURE_HALL, SOUTH);
+  room_t *lectureHallEast = initialiseRoom(LECTURE_HALL, EAST);
+  room_t *lectureHallWest = initialiseRoom(LECTURE_HALL, WEST);
+  room_t *lectureHallNorth = initialiseRoom(LECTURE_HALL, NORTH);
+  room_t *lectureHallCentre = initialiseRoom(LECTURE_HALL, CENTRE);
+
+  room_t *harrodsSouth = initialiseRoom(HARRODS, SOUTH);
+  room_t *harrodsEast = initialiseRoom(HARRODS, EAST);
+  room_t *harrodsWest = initialiseRoom(HARRODS, WEST);
+  room_t *harrodsNorth = initialiseRoom(HARRODS, NORTH);
+  room_t *harrodsCentre = initialiseRoom(HARRODS, CENTRE);
+
+  // connecting room positions with centre of rooms
+  connectRoomPositions(lobbySouth, lobbyEast, lobbyWest, lobbyNorth,
+                       lobbyCentre);
+  connectRoomPositions(labSouth, labEast, labWest, labNorth, labCentre);
+  connectRoomPositions(lectureHallSouth, lectureHallEast, lectureHallWest,
+                       lectureHallNorth, lectureHallCentre);
+  connectRoomPositions(fusionSouth, fusionEast, fusionWest, fusionNorth,
+                       fusionCentre);
+  connectRoomPositions(harrodsSouth, harrodsEast, harrodsWest, harrodsNorth,
+                       harrodsCentre);
 
   // connecting rooms which link to each other
-  connectRoom(lobby, lab);
-  connectRoom(lobby, lectureHall);
-  connectRoom(lobby, fusion);
-  connectRoom(fusion, harrods);
+  connectRoom(lobbyWest, labEast);
+  connectRoom(lobbyNorth, lectureHallSouth);
+  connectRoom(lobbyNorth, fusionSouth);
+  connectRoom(fusionNorth, harrodsSouth);
 
-  huxley->start_room = lobby;
+  huxley->start_room = lobbySouth;
 
   return huxley;
 }
