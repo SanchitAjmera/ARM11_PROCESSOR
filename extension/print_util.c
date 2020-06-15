1c4176cf4bf35b8fba10754dacf537962a548152
Luqman <lml19@imperial.ac.uk>
2020-06-15 19:26:47 +0100

    Refactored to move large print functions to end

--- extension/print_util.c
+++ extension/print_util.c
@@ -10,7 +10,134 @@
 
 #define CLEAR ("clear")
 
-// function for testing sleep() functions
+// funcion to print Room position
+void printRoomPosition(room_t *room) {
+  switch (room->position) {
+  case NORTH:
+    printf("NORTH ");
+    break;
+  case SOUTH:
+    printf("SOUTH ");
+    break;
+  case WEST:
+    printf("WEST ");
+    break;
+  case EAST:
+    printf("EAST ");
+    break;
+  case CENTRE:
+    printf("CENTRE ");
+    break;
+  }
+}
+
+void printItemDetails(item_t **items, int itemCount) {
+  if (items != NULL) {
+    for (int i = 0; i < itemCount; i++) {
+      printf("%s ", items[i]->key);
+    }
+    printf("\n");
+  }
+}
+
+// function for printing out name of room
+void printRoomName(room_t *room1) {
+
+  switch (room1->current_room) {
+  case LOBBY:
+    printf("LOBBY\n");
+    break;
+  case LAB:
+    printf("LAB\n");
+    break;
+  case HARRODS:
+    printf("HARRODS\n");
+    break;
+  case FUSION:
+    printf("FUSION\n");
+    break;
+  case LECTURE_HALL:
+    printf("LECTURE_HALL\n");
+    break;
+  }
+}
+
+// function for printing out names of adjacent rooms
+void printAdjacentRooms(room_t *room1) {
+  for (int i = 0; i < room1->adjacent_room_count; i++) {
+    printRoomPosition(room1->adjacent_rooms[i]);
+    printRoomName(room1->adjacent_rooms[i]);
+  }
+}
+
+// function purely for testing ouptut of connect RoomName
+void printRoomDetails(room_t *room1) {
+  printRoomPosition(room1);
+  printRoomName(room1);
+  printf("Items in the room: ");
+  printItemDetails(room1->Items, room1->ItemCount);
+  printf("number of adjacent rooms: %d\n", room1->adjacent_room_count);
+  printf("adjacent rooms:\n");
+  printAdjacentRooms(room1);
+}
+
+bool containsRoom(room_t *printedRooms[4], int size, room_t *room) {
+  for (int i = 0; i < size; i++) {
+    if (room->current_room == printedRooms[i]->current_room) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// traverses through all rooms and applies function pointer to each room
+void roomTraverser(room_t *entranceRoom, room_t *room1,
+                   void (*funcRooms)(room_t *)) {
+  if (room1 == NULL) {
+    return;
+  }
+  if (room1->adjacent_rooms != NULL) {
+    for (int i = 0; i < room1->adjacent_room_count; i++) {
+      if (room1->adjacent_rooms[i] != entranceRoom) {
+        roomTraverser(room1, room1->adjacent_rooms[i], funcRooms);
+      }
+    }
+  }
+  (*funcRooms)(room1);
+  printf("\n");
+}
+
+// prints details of all rooms in the building
+void printBuildingDetails(building_t *huxley) {
+  roomTraverser(NULL, huxley->start_room, printRoomDetails);
+}
+
+void printStateDetails(state *state1) {
+  printf("hi I am %s\n", state1->profile.username);
+  printf("score: %d\n", state1->profile.score);
+  printf("Health: %d\n", state1->player->health);
+  printf("Cash: %d\n", state1->player->cash);
+  printf("Items in inventory: ");
+  printItemDetails(state1->player->inventory, state1->player->itemCount);
+  printf("currently in room: ");
+  printRoomDetails(state1->curr_room_node);
+}
+
+//-------------------------Konstantinos-----------------------------------------
+
+void printRemaining(void) {
+  printf("                                          YOU");
+  fflush(stdout);
+  printf("       HAVE");
+  fflush(stdout);
+  printf("       10");
+  fflush(stdout);
+  printf("       MEEEENATES");
+  fflush(stdout);
+  printf("      REMAINING");
+  fflush(stdout);
+}
+
 void printKonstantinos(void) {
   system(CLEAR);
   printf("\n");
@@ -429,132 +556,7 @@ void printKonstantinosMouthOpenIndent(void) {
   printf("\n");
 }
 
-// funcion to print Room position
-void printRoomPosition(room_t *room) {
-  switch (room->position) {
-  case NORTH:
-    printf("NORTH ");
-    break;
-  case SOUTH:
-    printf("SOUTH ");
-    break;
-  case WEST:
-    printf("WEST ");
-    break;
-  case EAST:
-    printf("EAST ");
-    break;
-  case CENTRE:
-    printf("CENTRE ");
-    break;
-  }
-}
-
-void printItemDetails(item_t **items, int itemCount) {
-  if (items != NULL) {
-    for (int i = 0; i < itemCount; i++) {
-      printf("%s ", items[i]->key);
-    }
-    printf("\n");
-  }
-}
-
-// function for printing out name of room
-void printRoomName(room_t *room1) {
-
-  switch (room1->current_room) {
-  case LOBBY:
-    printf("LOBBY\n");
-    break;
-  case LAB:
-    printf("LAB\n");
-    break;
-  case HARRODS:
-    printf("HARRODS\n");
-    break;
-  case FUSION:
-    printf("FUSION\n");
-    break;
-  case LECTURE_HALL:
-    printf("LECTURE_HALL\n");
-    break;
-  }
-}
-
-// function for printing out names of adjacent rooms
-void printAdjacentRooms(room_t *room1) {
-  for (int i = 0; i < room1->adjacent_room_count; i++) {
-    printRoomPosition(room1->adjacent_rooms[i]);
-    printRoomName(room1->adjacent_rooms[i]);
-  }
-}
-
-// function purely for testing ouptut of connect RoomName
-void printRoomDetails(room_t *room1) {
-  printRoomPosition(room1);
-  printRoomName(room1);
-  printf("Items in the room: ");
-  printItemDetails(room1->Items, room1->ItemCount);
-  printf("number of adjacent rooms: %d\n", room1->adjacent_room_count);
-  printf("adjacent rooms:\n");
-  printAdjacentRooms(room1);
-}
-
-bool containsRoom(room_t *printedRooms[4], int size, room_t *room) {
-  for (int i = 0; i < size; i++) {
-    if (room->current_room == printedRooms[i]->current_room) {
-      return true;
-    }
-  }
-  return false;
-}
-
-// traverses through all rooms and applies function pointer to each room
-void roomTraverser(room_t *entranceRoom, room_t *room1,
-                   void (*funcRooms)(room_t *)) {
-  if (room1 == NULL) {
-    return;
-  }
-  if (room1->adjacent_rooms != NULL) {
-    for (int i = 0; i < room1->adjacent_room_count; i++) {
-      if (room1->adjacent_rooms[i] != entranceRoom) {
-        roomTraverser(room1, room1->adjacent_rooms[i], funcRooms);
-      }
-    }
-  }
-  (*funcRooms)(room1);
-  printf("\n");
-}
-
-// prints details of all rooms in the building
-void printBuildingDetails(building_t *huxley) {
-  roomTraverser(NULL, huxley->start_room, printRoomDetails);
-}
-
-void printStateDetails(state *state1) {
-  printf("hi I am %s\n", state1->profile.username);
-  printf("score: %d\n", state1->profile.score);
-  printf("Health: %d\n", state1->player->health);
-  printf("Cash: %d\n", state1->player->cash);
-  printf("Items in inventory: ");
-  printItemDetails(state1->player->inventory, state1->player->itemCount);
-  printf("currently in room: ");
-  printRoomDetails(state1->curr_room_node);
-}
-
-void printRemaining(void) {
-  printf("                                          YOU");
-  fflush(stdout);
-  printf("       HAVE");
-  fflush(stdout);
-  printf("       10");
-  fflush(stdout);
-  printf("       MEEEENATES");
-  fflush(stdout);
-  printf("      REMAINING");
-  fflush(stdout);
-}
-
+// function for testing sleep() functions
 void printKonstantinosTalking(void) {
   for (int i = 0; i < 18; i++) {
     if (i % 6 == 0 || i % 6 == 1 || i % 6 == 2) {
