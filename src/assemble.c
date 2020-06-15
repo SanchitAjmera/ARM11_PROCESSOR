7ad154f4853cb9fd8de7a72ca68788dea26e7129
ms1419 <ms1419@imperial.ac.uk>
2020-06-15 23:10:20 +0100

    added 0 to collisions field

--- src/assemble.c
+++ src/assemble.c
@@ -11,29 +11,29 @@ int main(int argc, char **argv) {
   symbol_table *symbolTable = newSymbolTable();
   symbol **pre = createSymbols(PREDEFINED_SYMBOLS_COUNT, sizeof(*pre));
   symbol predefinedSymbols[PREDEFINED_SYMBOLS_COUNT] = {
-      {strptr("add"), INSTR, .body.assembleFunc = assembleDPI},
-      {strptr("sub"), INSTR, .body.assembleFunc = assembleDPI},
-      {strptr("rsb"), INSTR, .body.assembleFunc = assembleDPI},
-      {strptr("and"), INSTR, .body.assembleFunc = assembleDPI},
-      {strptr("eor"), INSTR, .body.assembleFunc = assembleDPI},
-      {strptr("orr"), INSTR, .body.assembleFunc = assembleDPI},
-      {strptr("mov"), INSTR, .body.assembleFunc = assembleDPI},
-      {strptr("tst"), INSTR, .body.assembleFunc = assembleDPI},
-      {strptr("teq"), INSTR, .body.assembleFunc = assembleDPI},
-      {strptr("cmp"), INSTR, .body.assembleFunc = assembleDPI},
-      {strptr("mul"), INSTR, .body.assembleFunc = assembleMultiply},
-      {strptr("mla"), INSTR, .body.assembleFunc = assembleMultiply},
-      {strptr("ldr"), INSTR, .body.assembleFunc = assembleSDTI},
-      {strptr("str"), INSTR, .body.assembleFunc = assembleSDTI},
-      {strptr("beq"), INSTR, .body.assembleFunc = assembleBranch},
-      {strptr("bne"), INSTR, .body.assembleFunc = assembleBranch},
-      {strptr("bge"), INSTR, .body.assembleFunc = assembleBranch},
-      {strptr("blt"), INSTR, .body.assembleFunc = assembleBranch},
-      {strptr("bgt"), INSTR, .body.assembleFunc = assembleBranch},
-      {strptr("ble"), INSTR, .body.assembleFunc = assembleBranch},
-      {strptr("b"), INSTR, .body.assembleFunc = assembleBranch},
-      {strptr("lsl"), INSTR, .body.assembleFunc = assembleDPI},
-      {strptr("andeq"), INSTR, .body.assembleFunc = assembleDPI}};
+      {strptr("add"), INSTR, 0, .body.assembleFunc = assembleDPI},
+      {strptr("sub"), INSTR, 0, .body.assembleFunc = assembleDPI},
+      {strptr("rsb"), INSTR, 0, .body.assembleFunc = assembleDPI},
+      {strptr("and"), INSTR, 0, .body.assembleFunc = assembleDPI},
+      {strptr("eor"), INSTR, 0, .body.assembleFunc = assembleDPI},
+      {strptr("orr"), INSTR, 0, .body.assembleFunc = assembleDPI},
+      {strptr("mov"), INSTR, 0, .body.assembleFunc = assembleDPI},
+      {strptr("tst"), INSTR, 0, .body.assembleFunc = assembleDPI},
+      {strptr("teq"), INSTR, 0, .body.assembleFunc = assembleDPI},
+      {strptr("cmp"), INSTR, 0, .body.assembleFunc = assembleDPI},
+      {strptr("mul"), INSTR, 0, .body.assembleFunc = assembleMultiply},
+      {strptr("mla"), INSTR, 0, .body.assembleFunc = assembleMultiply},
+      {strptr("ldr"), INSTR, 0, .body.assembleFunc = assembleSDTI},
+      {strptr("str"), INSTR, 0, .body.assembleFunc = assembleSDTI},
+      {strptr("beq"), INSTR, 0, .body.assembleFunc = assembleBranch},
+      {strptr("bne"), INSTR, 0, .body.assembleFunc = assembleBranch},
+      {strptr("bge"), INSTR, 0, .body.assembleFunc = assembleBranch},
+      {strptr("blt"), INSTR, 0, .body.assembleFunc = assembleBranch},
+      {strptr("bgt"), INSTR, 0, .body.assembleFunc = assembleBranch},
+      {strptr("ble"), INSTR, 0, .body.assembleFunc = assembleBranch},
+      {strptr("b"), INSTR, 0, .body.assembleFunc = assembleBranch},
+      {strptr("lsl"), INSTR, 0, .body.assembleFunc = assembleDPI},
+      {strptr("andeq"), INSTR, 0, .body.assembleFunc = assembleDPI}};
   for (int i = 0; i < PREDEFINED_SYMBOLS_COUNT; i++) {
     pre[i][0] = predefinedSymbols[i];
   }
@@ -47,22 +47,11 @@ int main(int argc, char **argv) {
   printFileLines(fileLines);
   printSymbolTable(symbolTable);
 
-  printf("\n");
-
-  char buffer[512];
-  strcpy(buffer, "mov r1,#1\nadd r2,r1,#2\nfoo:\nldr r0,[r1,r2,lsl #2]\n");
-  char *delim = "[]";
-
-  char *word = strtok(buffer, delim);
-  while (word != NULL) {
-    printf("%s\n", word);
-    word = strtok(NULL, delim);
-  }
-
   FILE *binOutFile = fopen(argv[2], "wb");
   parseLines(fileLines, symbolTable, binOutFile);
   fclose(binOutFile);
 
+  freeSymbols(pre, PREDEFINED_SYMBOLS_COUNT);
   freeTable(symbolTable);
   freeFileLines(fileLines);
 
