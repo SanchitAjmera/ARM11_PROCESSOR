ed860571acd9758066837fc5cdbc25248f88e7d4
Alex R <arr19@ic.ac.uk>
2020-06-15 15:37:09 +0100

    Moved constants to 'boss_constants.h'

--- extension/characters/boss/boss.h
+++ extension/characters/boss/boss.h
@@ -1,22 +1,9 @@
 #ifndef BOSS_H
 #define BOSS_H
 
+#include "boss_constants.h"
 #include <stdbool.h>
 #include <string.h>
-
-#define MIN_QUESTIONS_CORRECT (3)
-#define MAX_QUESTIONS (5)
-#define BOSSES (2)
-#define KGK_ATTACK (15)
-#define KGK_SPECIAL (25)
-#define KGK_ATTACK_NAME ("10 mins remaining")
-#define KGK_SPECIAL_NAME ("LabTS is down")
-#define KGK_MAX_HEALTH (120)
-#define TONY_ATTACK (20)
-#define TONY_SPECIAL (1000)
-#define TONY_ATTACK_NAME ("Code style violations")
-#define TONY_SPECIAL_NAME ("Minus 1000")
-#define TONY_MAX_HEALTH (90)
 
 typedef struct {
   const char **questions;
