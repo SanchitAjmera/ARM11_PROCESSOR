a4452cc19465d988e5ff413766d587386dcc1a18
Alex R <arr19@ic.ac.uk>
2020-06-15 15:32:20 +0100

    Constants for battle.c

---
+++ extension/battle/battle_constants.h
@@ -0,0 +1,11 @@
+#ifndef BATTLE_CONSTANTS_H
+#define BATTLE_CONSTANTS_H
+
+#define BOSS_LOW_HEALTH(boss) (boss->fighting->maxHealth * 0.25)
+#define FIGHT(boss) (boss->fighting)
+#define CRIT_FACTOR (2)
+#define CRIT_CHANCE (8)
+#define CRIT_MAX_CHANCE (10)
+#define IS_CRIT(num) ((num % CRIT_MAX_CHANCE) > CRIT_CHANCE)
+
+#endif
