8a8bdb1252b87776b2a8b6e67c120d13d6551bd3
Luqman <lml19@imperial.ac.uk>
2020-06-15 19:12:11 +0100

    Used new quit command for quit case

--- extension/game.c
+++ extension/game.c
@@ -8,11 +8,13 @@
 #include <string.h>
 #include <unistd.h>
 
+// TODO:
 void playGame(state *currentState) { printf("Playing!\n"); }
 
 int main(void) {
 
   int choice = NONE;
+  char menuChoice[100];
 
   printf("Welcome to the ICL Text Adventure!\n");
   printf("What would you like to do?\n");
@@ -21,36 +23,42 @@ int main(void) {
   printf("    3. Load a previously saved game\n");
 
   while (choice != NEW_GAME && choice != LOAD_GAME && choice != QUIT) {
-    char menuChoice[100];
 
     printf("Please enter a 1 (quit), 2 (new game) or 3 (load game)!\n");
     printf("   >> ");
 
     fgets(menuChoice, sizeof(menuChoice), stdin);
-
     choice = atoi(menuChoice);
 
     switch (choice) {
     case QUIT:
-      printf("Thanks for playing!\n");
-      exit(EXIT_SUCCESS);
+      quit();
 
     case NEW_GAME:
       printf("Prepare for a new adventure...\n");
-      building_t *huxley = initialiseBuilding();
       // TODO: validate huxley ptr
+      building_t *huxley = initialiseBuilding();
       state *playerState = initialiseState(huxley->start_room);
-      // TODO: validate player ptr
       playGame(playerState);
+
+      printBuildingDetails(huxley);
+      printf("\n");
+      printStateDetails(playerState);
+
+      freeState(playerState);
+      freeBuilding(huxley);
+
       break;
 
     case LOAD_GAME:
       printf("Enter the load file name: ");
+
       char fileName[100];
       fgets(fileName, sizeof(fileName), stdin);
-      printf("File loaded: %s\n", fileName);
-      int result = -1; // loadGame(fileName); // to be checked
 
+      printf("File loaded: %s\n", fileName);
+
+      int result = -1; // loadGame(fileName); // to be checked
       if (result == -1) {
         printf("File loading failed!\n");
         printf("You have been taken back to the menu.\n");
@@ -64,15 +72,6 @@ int main(void) {
     }
   }
   printf("Done\n");
-  // printBuildingDetails(huxley);
-  //
-  // printf("\n");
-  //
-  // printStateDetails(playerState);
-  //
-  // freeState(playerState);
-  //
-  // freeBuilding(huxley);
 
   exit(EXIT_SUCCESS);
 }
