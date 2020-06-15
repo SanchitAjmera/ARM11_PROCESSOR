08afd46ad4ba2800ec94886e69bbd4cb7c1d570a
Alex R <arr19@ic.ac.uk>
2020-06-15 15:15:55 +0100

    Added macros for critical feature

--- extension/battle/battle.h
+++ extension/battle/battle.h
@@ -3,5 +3,9 @@
 
 #define BOSS_LOW_HEALTH(boss) (boss->fighting->maxHealth * 0.25)
 #define FIGHT(boss) (boss->fighting)
+#define CRIT_FACTOR (2)
+#define CRIT_CHANCE (8)
+#define CRIT_MAX_CHANCE (10)
+#define IS_CRIT(num) ((num % CRIT_MAX_CHANCE) > CRIT_CHANCE)
 
 #endif
