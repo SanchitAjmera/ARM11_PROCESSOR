f0641c6a8f65ecf18300de4e0483a207057450f4
Luqman <lml19@imperial.ac.uk>
2020-06-15 18:06:41 +0100

    Removed player type temporarily for testing

--- extension/characters/player/player.h
+++ extension/characters/player/player.h
@@ -1,14 +1,6 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-#include "game_util.h"
-
-// Struct to store details about player;
-typedef struct {
-  item_t **inventory;
-  int itemCount;
-  int health;
-  int cash;
-} player_t;
+#include "../../game_util.h"
 
 #endif
