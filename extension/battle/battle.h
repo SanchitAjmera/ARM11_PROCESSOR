daaee6d84a29c4779ddaa209e3d1617d24e3cf9f
Alex R <arr19@ic.ac.uk>
2020-06-15 15:31:56 +0100

    Moved constants to 'battle_constants.h'

--- extension/battle/battle.h
+++ extension/battle/battle.h
@@ -1,13 +1,6 @@
 #ifndef BATTLE_H
 #define BATTLE_H
 
-#define BOSS_LOW_HEALTH(boss) (boss->fighting->maxHealth * 0.25)
-#define FIGHT(boss) (boss->fighting)
-#define CRIT_FACTOR (2)
-#define CRIT_CHANCE (8)
-#define CRIT_MAX_CHANCE (10)
-#define IS_CRIT(num) ((num % CRIT_MAX_CHANCE) > CRIT_CHANCE)
-
 extern void battle(boss_t *boss, player_t *player);
 
 #endif
