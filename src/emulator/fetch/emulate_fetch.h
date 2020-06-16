56d13f58b343378e15ea943c886c641ea34bd7d8
arr19 <arr19@ic.ac.uk>
2020-06-16 16:45:45 +0100

    Changed var to camel case

--- src/emulator/fetch/emulate_fetch.h
+++ src/emulator/fetch/emulate_fetch.h
@@ -1,7 +1,7 @@
 #ifndef EMULATE_FETCH_H
 #define EMULATE_FETCH_H
 
-extern word getWord(byte *start_addr, bool isBigEndian);
+extern word getWord(byte *startAddress, bool isBigEndian);
 extern void fetch(arm_t *state);
 
 #endif
