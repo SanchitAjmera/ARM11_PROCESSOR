7ddcd87edfb27c760a81e16863a564b8bc025c27
arr19 <arr19@ic.ac.uk>
2020-06-16 15:12:47 +0100

    Added initArm

--- src/emulator/fetch/emulate_fetch.c
+++ src/emulator/fetch/emulate_fetch.c
@@ -2,6 +2,33 @@
 #include "../emulate_util.h"
 #inlcude "../../common/constants.h"
 #inlcude "../../common/util.h"
+#include "emulate_execute.h"
+#include <stdlib.h>
+
+/* Takes in the ARM binary file's name and returns an ARM state pointer with
+ * memory and register
+ * pointers on heap, where memory is of size MEMORY_CAPACITY bytes */
+void initArm(arm_t *state, const char *fname) {
+  // load binary file into memory
+  byte *memory = (byte *)calloc(MEMORY_CAPACITY, sizeof(byte));
+  errorExit(validatePtr(memory, "Not enough memory.\n"));
+  // file handling
+  FILE *binFile = fopen(fname, "rb");
+  errorExit(validatePtr(binFile, "File could not be opened\n"));
+  fseek(binFile, SEEK_SET, SEEK_END);
+  long fileSize = ftell(binFile);
+  rewind(binFile);
+  // Asserts that fread read the whole file
+  assert(fread(memory, 1, fileSize, binFile) == fileSize);
+  fclose(binFile);
+  // initialise registers
+  word *registers = (word *)calloc(NUM_REGISTERS, sizeof(word));
+
+  // construct ARM state
+  state->memory = memory;
+  state->registers = registers;
+  flushPipeline(state);
+}
 
 word getWord(byte *startAddress, bool isBigEndian) {
   word w = 0;
