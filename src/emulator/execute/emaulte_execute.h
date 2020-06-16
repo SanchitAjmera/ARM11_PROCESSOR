d9c09a206807024ca492494c0fb84fbadbbb27e9
arr19 <arr19@ic.ac.uk>
2020-06-16 15:22:33 +0100

    Removed redudant files

--- src/emulator/execute/emaulte_execute.h
+++
@@ -1,6 +0,0 @@
-#ifndef EMULATE_EXECUTE_H
-#define EMULATE_EXECUTE_H
-
-extern void execute(arm_t *state, decoded_t *decoded);
-
-#endif
--- src/emulator/fetch/emaulte_fetch.h
+++
@@ -1,7 +0,0 @@
-#ifndef EMULATE_FETCH_H
-#define EMULATE_FETCH_H
-
-extern word getWord(byte *start_addr, bool isBigEndian);
-extern void fetch(arm_t *state);
-
-#endif
