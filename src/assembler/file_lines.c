c735026830efaf4b7f5a8a8bfebded0caa4d6e02
arr19 <arr19@ic.ac.uk>
2020-06-17 22:14:00 +0100

    Changed struct file_lines to fileLines_t

--- src/assembler/file_lines.c
+++ src/assembler/file_lines.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void addLine(file_lines *fl, const char *line) {
+void addLine(fileLines_t *fl, const char *line) {
   if (fl->lineCount == fl->maxLines) {
     fl->lines = realloc(fl->lines, fl->maxLines *= 2);
     validatePtr(fl->lines, MEM_ASSIGN);
@@ -15,22 +15,22 @@ void addLine(file_lines *fl, const char *line) {
 }
 
 /* Adds each line into file line structure from string array */
-void addLines(file_lines *fl, char **lines, uint n) {
+void addLines(fileLines_t *fl, char **lines, uint n) {
   for (int i = 0; i < n; i++) {
     addLine(fl, lines[i]);
   }
 }
 
 /* Outputs each line stored in the file lines structure */
-void printFileLines(file_lines *fileLines) {
+void printFileLines(fileLines_t *fileLines) {
   for (int i = 0; i < fileLines->lineCount; i++) {
     printf("%i: %s\n", i, fileLines->lines[i]);
   }
 }
 
 /* Initialising the structure to store each line of instruction file */
-file_lines *newFileLines() {
-  file_lines *fileLines = malloc(sizeof(*fileLines));
+fileLines_t *newFileLines(void) {
+  fileLines_t *fileLines = malloc(sizeof(*fileLines));
   validatePtr(fileLines, MEM_ASSIGN);
   fileLines->maxLines = INIT_FILE_LINES_SIZE;
   fileLines->lineCount = 0;
@@ -40,7 +40,7 @@ file_lines *newFileLines() {
 }
 
 /* Free resources used for storing program file lines */
-void freeFileLines(file_lines *fileLines) {
+void freeFileLines(fileLines_t *fileLines) {
   for (int i = 0; i < fileLines->lineCount; i++) {
     free(fileLines->lines[i]);
   }
--- src/assembler/file_lines.h
+++ src/assembler/file_lines.h
@@ -7,12 +7,12 @@
 typedef struct {
   char **lines;
   uint lineCount, maxLines;
-} file_lines;
+} fileLines_t;
 
-extern void addLine(file_lines *fl, const char *line);
-extern void addLines(file_lines *fl, char **lines, uint n);
-extern void printFileLines(file_lines *fileLines);
-extern file_lines *newFileLines();
-extern void freeFileLines(file_lines *fileLines);
+extern void addLine(fileLines_t *fl, const char *line);
+extern void addLines(fileLines_t *fl, char **lines, uint n);
+extern void printFileLines(fileLines_t *fileLines);
+extern fileLines_t *newFileLines(void);
+extern void freeFileLines(fileLines_t *fileLines);
 
 #endif
