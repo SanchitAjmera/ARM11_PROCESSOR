6cab760ba72f400371b3fb532a92b95bde7c12bd
ms1419 <ms1419@imperial.ac.uk>
2020-06-17 22:05:11 +0100

    implemented the playerTurn function

--- extension/battle/battle.c
+++ extension/battle/battle.c
@@ -1,7 +1,10 @@
 #include "battle.h"
 #include "../characters/boss/boss.h"
 #include "../characters/player/player.h"
+#include "../emulateARM/emulateARM.h"
+#include "../game_util.h"
 #include "battle_constants.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -38,10 +41,76 @@ static void attackBoss(boss_t *boss, player_t *player, int damage,
   printAttack("You", damage, attackName);
 }
 
+typedef struct {
+  char *value;
+  int length, maxLength;
+} resizable_string;
+
+void extendString(resizable_string *string) {
+  if (string->length < string->maxLength - 1) {
+    return;
+  }
+  while (string->length >= string->maxLength - 1) {
+    string->maxLength *= 2;
+  }
+  string->value = realloc(string->value, sizeof(char) * string->maxLength);
+}
+
+resizable_string *newString() {
+  resizable_string *string = malloc(sizeof(resizable_string));
+  string->value = malloc(sizeof(char));
+  string->length = 0;
+  string->maxLength = 16;
+  extendString(string);
+  return string;
+}
+
+void appendToString(resizable_string *string, const char *append) {
+  int oldLength = string->length;
+  int appendLength = strlen(append);
+  string->length += appendLength;
+  extendString(string);
+  char *startPos = string->value + oldLength;
+  strcpy(startPos, append);
+  strcpy(string->value + string->length, "\0");
+}
+
+void freeString(resizable_string *string) {
+  free(string->value);
+  free(string);
+}
+
 // player's turn in battle
-static void playerTurn(boss_t *boss, player_t *player) {
-  // TODO: @Luqman to code...
+void playerTurn(boss_t *boss, player_t *player) {
+  bool takeInput = true;
+  resizable_string *code = newString();
+
+  char *input = malloc(sizeof(char) * 50);
+  while (takeInput) {
+    printf("                     >> ");
+    fgets(input, sizeof(char) * 50, stdin);
+
+    // Check if "END" was typed in
+    char *inputCopy = strptr(input);
+    char *token = strtok(inputCopy, " \n");
+    if (!strcmp(token, "END")) {
+      takeInput = false;
+      free(inputCopy);
+      break;
+    }
+    free(inputCopy);
+
+    appendToString(code, input);
+  }
+  printf("%s\n", code->value);
+  char **output = runCode(code->value);
+  printOutput(output);
+
   attackBoss(boss, player, 1, "sanchizzle");
+
+  freeOutput(output);
+  freeString(code);
+  free(input);
 }
 
 // boss's turn in battle
