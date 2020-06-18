ef693061fa55d6306ffbd02204e48991ccdee328
arr19 <arr19@ic.ac.uk>
2020-06-18 13:17:27 +0100

    Added extern keyword to functions

--- extension/print_utils/print_util.h
+++ extension/print_utils/print_util.h
@@ -3,21 +3,22 @@
 
 #include "game_util.h"
 
-void printRoomName(room_t *room1);
-void printAdjacentRooms(room_t *room1);
-void printRoomDetails(room_t *room1);
-void printItemDetails(item_t **items);
-void printBuildingDetails(building_t *huxley);
-void printKonstantinos(void);
-void printKonstantinosMouthOpen(void);
-void printKonstantinosIndent(void);
-void printKonstantinosMouthOpenIndent(void);
-void printRemaining(void);
-void printStateDetails(state *state1);
-void printMenu(bool isValid);
-void printInventory(state *currentState);
-void view(state *currentState, char *argument);
-void printPreparingGame(void);
-void printInvalid(void);
-void printMap(state *currentState);
+extern void printRoomName(room_t *room1);
+extern void printAdjacentRooms(room_t *room1);
+extern void printRoomDetails(room_t *room1);
+extern void printItemDetails(item_t **items);
+extern void printBuildingDetails(building_t *huxley);
+extern void printKonstantinos(void);
+extern void printKonstantinosMouthOpen(void);
+extern void printKonstantinosIndent(void);
+extern void printKonstantinosMouthOpenIndent(void);
+extern void printRemaining(void);
+extern void printStateDetails(state *state1);
+extern void printMenu(bool isValid);
+extern void printInventory(state *currentState);
+extern void view(state *currentState, char *argument);
+extern void printPreparingGame(void);
+extern void printInvalid(void);
+extern void printMap(state *currentState);
+
 #endif
